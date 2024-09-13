79eb1a83dddcf236ff624236488fbfbe76198fb4
kentongelis <kenton.gelis@students.dominican.edu>
2024-09-13 12:56:19 -0700

    fixed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+const { Deck } = require('kenton-doc') 
 
 function App() {
+  const deck = new Deck()
+  const deckEmojis = (deck) => {
+    let newList = []
+    for ( let card of deck ) {
+        newList.push(card.toEmoji())
+    }
+    return newList
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Deck</h1>
+      <div className='deck'>
+        {deckEmojis(deck.deck)}
+      </div>
+      {deck.shuffle()}
+      <h1>Shuffled Deck</h1>
+      <div className='deck'>
+        {deckEmojis(deck.deck)}
+      </div>
+      <div className='deck'>
+        {deck.discard(deck.draw())}
+      </div>
+      <h1>Deck after discard method</h1>
+      <div className='deck'>
+      {deckEmojis(deck.deck)}
+      </div>
+      <h1>Discard Pile</h1>
+      <div className='deck'>
+      {deckEmojis(deck.discardPile)}
+      </div>
+      <div className='deck'>
+      {deck.addCard(deck.discardPile[0])}
+      </div>
+      <h1>Deck after addCard method</h1>
+      <div className='deck'>
+      {deckEmojis(deck.deck)}
+      </div>
+      <div className='deck'>
+        {deck.discard(deck.draw())}
+      </div>
+      <div className='deck'>
+      {deck.shuffleCard(deck.discardPile[0])}
+      </div>
+      <h1>Deck after shuffleCard method</h1>
+      <div className='deck'>
+      {deckEmojis(deck.deck)}
+      </div>
+      <div className='deck'>
+        {deck.discard(deck.draw())}
+      </div>
+      <div className='deck'>
+        {console.log(deck.discardPile)}
+      {deck.slipCard(deck.discardPile[2])}
+      </div>
+      <h1>Deck after slipCard method</h1>
+      <div className='deck'>
+      {deckEmojis(deck.deck)}
+      </div>
+      <h1>Reveal Method</h1>
+      <div className='deck'>
+      {deck.reveal().toEmoji()}
+      </div>
+
+      
+    </main>
   );
 }
 
